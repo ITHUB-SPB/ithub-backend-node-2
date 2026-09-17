9d0348510598ba8ef0aa0b309da59f7a02a7a7dc
Трофимов Алексей <a.trofimov@domain.local>
2026-09-17 16:07:41 +0300

    17.09.2026:2

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,29 +1,74 @@
-import { Router } from "express"
+import { Router, type Response } from "express"
 import { users } from "../data.js"
+
+// TODO: быть построже с типами formatSuccess
+function formatSuccess(
+    response: Response,
+    data: { [k: string]: object },
+    code: number
+) {
+    response.status(code).json({
+        success: true,
+        data
+    })
+}
+
+// типизировать code либо через енам всех доступных кодов, 
+// либо через суживание типа number до 4xx и 5xx
+function formatError(
+    response: Response,
+    message: string,
+    code: number = 400,
+    details: object = {}
+) {
+    response.status(code).json({
+        success: false,
+        error: message,
+        details
+    })
+}
 
 export const usersRouter = Router()
 
 usersRouter.get('/users', (request, response) => {
-    const limit = request.query['limit'] || 20
-    const offset = request.query['offset'] || 0
+    const limit = Number(request.query['limit'] || 20)
+    const offset = Number(request.query['offset'] || 0)
 
-    response.json({
-        users: users.slice(Number(offset), Number(offset) + Number(limit))
-    })
+    const data = {
+        users: users.slice(offset, offset + limit)
+    }
+
+    const meta = {
+        total: users.length,
+        page: Math.ceil(users.length / (limit + offset)), // TODO
+        limit,
+        pages: Math.ceil(users.length / limit)
+    }
+
+    formatSuccess(response, { ...data, meta }, 200)
 })
 
 usersRouter.post('/users', (request, response) => {
     users.push(request.body)
 
-    response.status(201).json({
-        message: `Пользователь ${request.body.username} создан`
-    })
+    const data = { user: users.at(-1)! }
+
+    formatSuccess(response, data, 201)
 })
 
 usersRouter.get('/users/:username', (request, response) => {
-    response.json({
-        user: users.find(
-            u => u.username === request.params.username
-        )
-    })
+    const user = users.find(
+        u => u.username === request.params.username
+    )
+
+    if (!user) {
+        formatError(response, "Пользователь не найден", 404)
+        return
+    }
+
+    const data = {
+        user
+    }
+
+    formatSuccess(response, data, 200)
 })
