ac89645931ba9c00285d945e5f9be7807fb34fa1
Мельник Тимур <t.melnik@domain.local>
2026-09-24 18:09:35 +0300

    ada

--- src/data.ts
+++ src/data.ts
@@ -8,5 +8,27 @@ export const products = [
         "description": "Смачный белый батон",
         "imageUrl": "",
         "createdAt": ""
+    },
+
+    {
+        "id": 2,
+        "name": "Shapka",
+        "price": 231,
+        "category": "clothing",
+        "stock": 23,
+        "description": "for summer",
+        "imageUrl": "",
+        "createdAt": ""
+    },
+
+    {
+        "id": 3,
+        "name": "iphon",
+        "price": 4234,
+        "category": "electronics",
+        "stock": 23,
+        "description": "phone",
+        "imageUrl": "",
+        "createdAt": ""
     }
 ]
--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,47 @@ import { products } from "./data.js"
 z.config(ru())
 
 const app = express()
-app.get('/api/products', (_, response) => {
-    response.json(products)
+
+app.get('/api/products', (request, response) => {
+    let result = products;
+
+    const { category, minPrice, maxPrice, page, limit, offset } = request.query;
+
+    if (category) {
+        result = result.filter(p => p.category === String(category));
+    }
+    if (minPrice) {
+        result = result.filter(p => p.price >= Number(minPrice));
+    }
+    if (maxPrice) {
+        result = result.filter(p => p.price <= Number(maxPrice));
+    }
+
+    const total = result.length;
+
+    const limitNum = Number(limit) || 10;// кол-во товаров на странице (по умолчанию 10)
+    let pageNum, offsetNum;
+
+    if (offset) {
+        offsetNum = Number(offset);
+        pageNum = Math.floor(offsetNum / limitNum) + 1;
+    } else {
+        pageNum = Number(page) || 1;
+        offsetNum = (pageNum - 1) * limitNum;
+    }
+
+    const paginated = result.slice(offsetNum, offsetNum + limitNum);
+
+    response.json({ 
+        data: paginated,
+        total,
+        page: pageNum,
+        limit: limitNum
+    });
 })
 
 // подключение раздачи статики по виртуальному пути /static из директории /assets
+// TODO
 
 // встроенные глобальные миддлвэа на парсинг тел в json и x-www-form-urlencoded
 // TODO
