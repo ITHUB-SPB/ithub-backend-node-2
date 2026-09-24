fe3af570c6b59cb9ca096b7bdae5bd0ab03a0e7e
Трофимов Алексей <a.trofimov@domain.local>
2026-09-24 13:12:25 +0300

    24.09.2026

--- src/routes/media.ts
+++ src/routes/media.ts
@@ -1,10 +1,25 @@
+import path from 'node:path'
 import { Router, type Request, type Response } from "express";
 import multer from "multer";
 
-const uploadMiddleware = multer({
+const uploadMiddlewareMemory = multer({
     storage: multer.memoryStorage(),
     limits: { fileSize: 1024 * 1024 * 2 }
 })
+
+const uploadMiddlewareDisk = multer({
+    storage: multer.diskStorage({
+        destination: path.join(import.meta.dirname, '..', '..', 'assets', 'media'),
+        filename: (_, file, callback) => {
+            const extension = path.extname(file.originalname)
+            const nameWithoutExtension = path.parse(file.originalname).name
+            const name = `${nameWithoutExtension}_${Date.now()}${extension}`
+            callback(null, name)
+        }
+    }),
+    limits: { fileSize: 1024 * 1024 * 2 }
+})
+
 
 export const mediaRouter = Router()
 
@@ -12,7 +27,7 @@ mediaRouter.get('/media', (request: Request, response: Response) => {
     response.json()
 })
 
-mediaRouter.post('/media/upload', (request: Request, response: Response) => {
+mediaRouter.post('/media/upload', uploadMiddlewareDisk.single('document'), (request: Request, response: Response) => {
     console.log(request.file)
     response.end()
 })
