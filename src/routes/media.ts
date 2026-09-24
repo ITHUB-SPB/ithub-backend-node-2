ee2a346ecc6ffd494f024b55fb88a805323502e9
Трофимов Алексей <a.trofimov@domain.local>
2026-09-24 13:46:03 +0300

    24.09.2026

--- src/routes/media.ts
+++ src/routes/media.ts
@@ -1,6 +1,8 @@
 import path from 'node:path'
 import { Router, type Request, type Response } from "express";
+import * as mime from 'mime-types'
 import multer from "multer";
+
 
 const uploadMiddlewareMemory = multer({
     storage: multer.memoryStorage(),
@@ -17,9 +19,23 @@ const uploadMiddlewareDisk = multer({
             callback(null, name)
         }
     }),
-    limits: { fileSize: 1024 * 1024 * 2 }
-})
+    limits: {
+        fileSize: 1024 * 1024 * 2,
+        files: 3
+    },
+    fileFilter: (_, file, callback) => {
+        const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/webp', 'text/plain']
+        const realMimeType = mime.lookup(file.originalname)
 
+        if (!realMimeType) {
+            callback(new Error('Недопустимый mimeType'))
+        } else if (allowedMimeTypes.includes(realMimeType)) {
+            callback(null, true)
+        } else {
+            callback(new Error('Недопустимый mimeType'))
+        }
+    }
+})
 
 export const mediaRouter = Router()
 
@@ -27,7 +43,14 @@ mediaRouter.get('/media', (request: Request, response: Response) => {
     response.json()
 })
 
-mediaRouter.post('/media/upload', uploadMiddlewareDisk.single('document'), (request: Request, response: Response) => {
-    console.log(request.file)
-    response.end()
-})
+mediaRouter.post(
+    '/media/upload',
+    uploadMiddlewareDisk.fields([
+        { name: "document", maxCount: 2 },
+        { name: "avatar", maxCount: 1 }
+    ]),
+    (request: Request, response: Response
+    ) => {
+        console.log(request.file)
+        response.end()
+    })
